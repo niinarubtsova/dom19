8fe05b7ccee3743083f5b66365374e67834791f5
Niina <niinarubtsova@gmail.com>
2023-02-03 12:49:32 +0200

    new

--- models.py
+++ models.py
@@ -1,6 +1,34 @@
+import hashlib
+
 from marshmallow import Schema, fields
 
 from setup_db import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+    role = db.Column(db.String)
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        self.password = self.get_hash()
+
+    def get_hash(self):
+        return hashlib.md5(self.password.encode('utf-8')).hexdigest()
+
+    def compare_passwords(self, other_password) -> bool:
+        new_password_hash = hashlib.md5(other_password.encode('utf-8')).hexdigest()
+        return new_password_hash == self.password
+
+
+class UserSchema(Schema):
+    id = fields.Int()
+    username = fields.Str()
+    password = fields.Str()
+    role = fields.Str()
 
 
 class Movie(db.Model):
